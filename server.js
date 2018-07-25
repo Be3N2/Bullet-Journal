b2bf78ea6e393391cdc77f6926f6ce15ea6eae76
Be3N2 <2017WakefieldB@gmail.com>
2018-07-25 13:28:00 -0400

    Getting started with mongoose

--- server.js
+++ server.js
@@ -1,95 +1,26 @@
 var express = require('express');
 var app = express();
 
-var mongodb = require('mongodb');
-var MONGODB_URI = process.env.DBURL;
+var mongoose = require('mongoose');
 
-MONGODB_URI="mongodb://be3n2:learnmongoPass1@ds117615.mlab.com:17615/learningmongo";
 var COLLECTION = "bulletjournal";
 var PORT = 3000;
-
-var collection;
-var startup = true;
 
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
-	//if (startup) connect();
-	console.log(__dirname);
 	response.sendFile(__dirname + "/" + 'index.html');
 });
 
-//app.get("/save", fetch);
+app.get("/save", saveData());
 
 function saveData(request, response) {
-
-}
-
-function fetch(requst, response) {
-	if (startup) connect();
-
-	//going to want user to determine what object id is wanted
-	mongodb.MongoClient.connect(MONGODB_URI, function(err, db) {
-    if (err) {
-      //console.log('Unable to connect to the mongoDB server. Error:', err);
-      return;
-    }
-    
-    //console.log('Connection established to learningmongo database');
-    
-    // do some work here with the database.
-    collection = db.collection(process.env.COLLECTION);
-    collection.find().forEach(function(doc, err) {
-      if (err) throw err;
-      console.log(doc); //this is for outputting server contents
-    }, function() {
-      //callback function
-      startup = false;
-    });
-    
-    //database stats if needed
-    //var promiseObj = db.stats();
-    
-    //promiseObj.then(function(val) {
-    //  console.log(val);
-    //}); 
-
-  });
-}
-
-// open port with a callback
-var listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
-//mongo stuff
-function connect() {
-  
-  mongodb.MongoClient.connect(MONGODB_URI, function(err, db) {
-    if (err) {
-      //console.log('Unable to connect to the mongoDB server. Error:', err);
-      return;
-    }
-    
-    //console.log('Connection established to learningmongo database');
-    
-    // do some work here with the database.
-    collection = db.collection(process.env.COLLECTION);
-    collection.find().forEach(function(doc, err) {
-      if (err) throw err;
-      //console.log(doc); //this is for outputting server contents
-    }, function() {
-      //callback function
-      startup = false;
-    });
-    
-    //database stats if needed
-    //var promiseObj = db.stats();
-    
-    //promiseObj.then(function(val) {
-    //  console.log(val);
-    //}); 
-
-  });
-                              
+	mongoose.connect('mongodb://be3n2:learnmongoPass1@ds117615.mlab.com:17615/learningmongo');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function() {
+	  // we're connected!
+	  console.log("All data:");
+	  db.collectionName.find().pretty()
+	});
 }
